1f8880537b43dc27ea6722bf11e4505af6761bc9
bigZmmm <2396702709@qq.com>
2024-04-11 22:36:32 +0800

    20240411

--- extract_counterexample.c
+++ extract_counterexample.c
@@ -16,13 +16,67 @@
 #include "search.h"
 #include "extract_counterexample.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <memory.h>
 #include <setjmp.h>
-#include <stdio.h>
+
 #include "z3.h"
 #include "set.h"
+
+bool inFacts(int *nums,int n,int index){
+  int i;
+  for(i=0;i<n;i++){
+    if(index==nums[i]){
+      return true;
+    }
+  }
+    
+      
+  return false;
+}
+
+char* itoa(int num,char* str,int radix)
+{
+    char index[]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    unsigned unum;
+    int i=0,j,k;
+    if(radix==10&&num<0)
+    {
+        unum=(unsigned)-num;
+        str[i++]='-';
+    }
+    else unum=(unsigned)num;
+ 
+    do
+    {
+        str[i++]=index[unum%(unsigned)radix];
+        unum/=radix;
+ 
+    }while(unum);
+ 
+    str[i]='\0';
+ 
+
+    if(str[0]=='-') k=1;
+    else k=0;
+ 
+    char temp;
+    for(j=k;j<=(i-1)/2;j++)
+    {
+        temp=str[j];
+        str[j]=str[i-1+k-j];
+        str[i-1+k-j]=temp;
+    }
+ 
+    return str;
+ 
+}
+
+void expandString(char* cur,int n){
+  cur = (char *)realloc(cur, strlen(cur)+n);
+}
 
 char *Fact2SmtString(int i)
 {
@@ -48,29 +102,121 @@ char *Fact2SmtString(int i)
 }
 
 char* contactString(char *now,char *add){
-    int total_length = strlen(add) + strlen(now)+1;
-    now = (char *)realloc(now, total_length);
-    strcat(now, add);
-    strcat(now," ");
-    char *new_var = calloc(total_length,sizeof(char));
-    strcpy(new_var,now);
-    free(add);
-    free(now);
-    printf("\n%s\n",new_var);
+    int total_length = strlen(add) + strlen(now)+2;
+    char *new_var = (char*)calloc(total_length,sizeof(char));
+    strcat(new_var,now);
+    strcat(new_var," ");
+    strcat(new_var,add);
+    /*printf("\n%s\n",new_var);*/
     return new_var;
 }
 
 char* toSmtVariableString(char *now,int timestep){
-  char str[20]={0}; // 假设要转换的数字不超过 20 个字符的长度
-  str[0]='-';
-  sprintf(str+1, "%d", timestep);
+  char *str=(char *)calloc(20,sizeof(char));
+  itoa(timestep,str,10);
   int oldlen = strlen(now);
-  char *new_var = (char *)calloc(oldlen+strlen(str),sizeof(char) );
+  char *new_var = (char *)calloc(oldlen+strlen(str)+2,sizeof(char) );
   strcpy(new_var,now);
+  strcat(new_var,"-");
   strcat(new_var,str);
   free(now);
+  free(str);
   return new_var;
 }
+
+void addAction2Goal(char *simulation,int current_goal_fact,Action *a,int new_len,SimpleSet *regre_variable,int *new_goal,int timestep){
+    
+    print_ft_name(current_goal_fact);
+    
+    int i,e;
+    char *fact2string = toSmtVariableString(Fact2SmtString(current_goal_fact),timestep);
+    char *cur =  toSmtVariableString(Fact2SmtString(current_goal_fact),timestep-1);
+    char *pre = (char*)calloc(1,sizeof("(= ")+strlen(fact2string)+5);
+    char *to_smt = (char*)calloc(1,sizeof("(OR "));
+    char *add_to_string = (char*)calloc(1,sizeof("(OR FALSE "));
+    char *notdel_to_string = (char*)calloc(1,sizeof("(NOT (OR FALSE "));
+    strcat(to_smt,"(OR");
+    strcat(add_to_string,"(OR FALSE");
+    strcat(notdel_to_string,"(NOT (OR FALSE");
+    strcat(pre,"(= ");
+    strcat(pre,fact2string);
+    
+    for(e=0;e<(a->num_effects);e++){
+      ActionEffect ac = a->effects[e];
+      /*生成add的smt*/
+      /*success*/
+      if(inFacts(ac.adds,ac.num_adds,current_goal_fact)){
+        if ((ac.num_conditions) > 0){
+          if ((ac.num_conditions) > 1){
+            add_to_string=contactString(add_to_string, "(AND");
+            for (i = 0; i < ac.num_conditions; i++){
+              char *tmp = toSmtVariableString(Fact2SmtString(ac.conditions[i]),timestep-1);
+              /*添加新的变量*/
+              /*set_add(&regre_variable, tmp);*/
+              add_to_string=contactString(add_to_string, tmp);
+            }
+          }
+          else{printf("\n1111111112%d\n",ac.conditions[0]);
+            char *tmp = toSmtVariableString(Fact2SmtString(ac.conditions[0]), timestep-1);
+            printf("\n%s",tmp);
+            /*添加新的变量*/
+            /*set_add(&regre_variable, tmp);*/
+            add_to_string=contactString(add_to_string, tmp);
+          }
+        }else{
+          add_to_string=contactString(add_to_string, "TRUE");
+        }
+      /*生成del的smt*/
+      }else if(inFacts(ac.dels,ac.num_dels,current_goal_fact)){
+        if((ac.num_conditions)>0){
+          if((ac.num_conditions)>1){
+              notdel_to_string=contactString(notdel_to_string,"(AND");
+              for(i=0;i<ac.num_conditions;i++){
+                char *tmp = toSmtVariableString(Fact2SmtString(ac.conditions[i]),timestep-1);
+                /*添加新的变量*/
+                set_add(&regre_variable, tmp);
+                notdel_to_string=contactString(notdel_to_string,tmp);
+              }
+          }else{
+            char *tmp = toSmtVariableString(Fact2SmtString(ac.conditions[0]),timestep-1);
+            /*添加新的变量*/
+            set_add(&regre_variable, tmp);
+            notdel_to_string=contactString(notdel_to_string,tmp);
+          }
+        }else{
+          notdel_to_string=contactString(notdel_to_string, "TRUE");
+        }
+      }
+    }
+    
+    add_to_string= contactString(add_to_string, ")");
+    notdel_to_string= contactString(notdel_to_string,") )");
+
+    to_smt = contactString(to_smt,"(AND");
+    to_smt = contactString(to_smt,notdel_to_string);
+    to_smt = contactString(to_smt,add_to_string);
+    to_smt = contactString(to_smt,")");
+    to_smt = contactString(to_smt,"(AND");
+    to_smt = contactString(to_smt,cur);
+    to_smt = contactString(to_smt,notdel_to_string);
+    to_smt = contactString(to_smt,") )");
+    pre = contactString(pre,to_smt);
+    pre = contactString(pre,")");
+    /*printf("\n%s\n",simulation);*/
+    
+    simulation = contactString(simulation,pre);
+    
+    free(fact2string);
+    free(cur);
+    free(pre);
+    free(to_smt);
+    free(add_to_string);
+    free(notdel_to_string);
+    printf("\n: %s \n",simulation);
+    
+}
+
+
 
 Bool conputerCounter()
 {
@@ -113,33 +259,36 @@ Bool conputerCounter()
   
   */
   int timestep = gnum_plan_ops;
-  /*存储当前的目标状态谓语*/
+  /*存储当前的目标状态谓语*/  /*一*/
   int goal_len=ggoal_state.num_F+ggoal_state.num_U;
   int *current_goal_fact=(int *)calloc(goal_len, sizeof(int));
   for(i=0;i<ggoal_state.num_F;i++){
     current_goal_fact[i]=ggoal_state.F[i];
     preference=contactString(preference,toSmtVariableString(Fact2SmtString(ggoal_state.F[i]),timestep));
   }
+  
   for(i=0;i<ggoal_state.num_U;i++){
     current_goal_fact[ggoal_state.num_F+i]=ggoal_state.U[i];
     preference=contactString(preference,toSmtVariableString(Fact2SmtString(ggoal_state.U[i]),timestep));
   }
 
-  /*一*/
-  printf("\n%s %d\n",preference,strlen(preference));
-
-  
-
-  /*当前目标状态ggoal_state*/
+  /*当前目标状态ggoal_state
+  对目标状态进行回归*/
+  SimpleSet regre_variable;
+  int new_goal[10000];
+  for (i = timestep-1; i >=0; i--)
   {
-    
-    for (i = timestep; i > 0; i--)
-    {
-
-
-    }
+      printf("\n%d :",i);
+      print_op_name( gplan_ops[i] );
+      Action *a = gop_conn[gplan_ops[i]].action;
+      int new_len=0;
+      for(j=0;j<goal_len;j++){
+          printf("\n%s\n",simulation);
+          addAction2Goal(simulation,current_goal_fact[j],a,&new_len,&regre_variable,new_goal,i+1);
+      }
+      /*goal_len = new_len;*/
   }
-
+  
   /*
   for ( i = 0; i < gnum_plan_ops; i++ ) {
     printf("%d: ", i);
